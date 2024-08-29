2febf259a63efc87e4282d6d42d5f1c5b5d8030a
Ivan Tandella <ivantandella2@gmail.com>
2024-08-29 09:32:09 +0700

    add variant props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,15 @@ type ButtonProps = {
   children: React.ReactNode;
   type?: "submit" | "reset" | "button";
   onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  variant?: string;
 };
 
 export default function Button(props: ButtonProps) {
-  const { children, type = "button", onClick } = props;
+  const { children, type = "button", onClick, variant = "bg-blue-600" } = props;
   return (
     <button
       type={type}
-      className="h-10 px-6 font-semibold bg-blue-600 text-white rounded"
+      className={"h-10 px-6 font-semibold text-white rounded" + " " + variant}
       onClick={onClick}
     >
       {children}
