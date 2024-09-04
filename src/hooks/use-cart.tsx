6a1a8094b1f2f0dd97354d09c86d6f03071f8670
Ivan Tandella <ivantandella2@gmail.com>
2024-09-04 09:18:49 +0700

    refactor products page

--- src/hooks/use-cart.tsx
+++ src/hooks/use-cart.tsx
@@ -1,15 +1,29 @@
 import { useImmer } from "use-immer";
-import { CartType } from "../pages/products";
 import { getProducts, ProductType } from "../services/product.service";
-import React from "react";
+import { useEffect, useState } from "react";
+import { useLogin } from "./useLogin";
+import useTotalPrice from "./use-total-price";
+
+export type CartType = {
+  id: number;
+  title: string;
+  quantity: number;
+};
 
 export default function useCart() {
   const [cart, updateCart] = useImmer<CartType[]>([]);
-  const [totalPrice, setTotalPrice] = React.useState(0);
-  const [isSync, setIsSync] = React.useState(false);
-  const [products, setProducts] = React.useState<ProductType[]>([]);
+  const [isSync, setIsSync] = useState(false);
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const { totalPrice, onUpdateTotalPrice } = useTotalPrice();
 
-  React.useEffect(() => {
+  // const [totalPrice, setTotalPrice] = useState(0);
+  // const dispatch = useTotalPriceDispatch();
+  // const { total } = useTotalPrice();
+
+  useLogin();
+
+  // get product from api
+  useEffect(() => {
     async function exec() {
       try {
         const data = await getProducts();
@@ -20,12 +34,14 @@ export default function useCart() {
       }
     }
     exec();
+
     // getProducts((data) => {
     //   setProducts(data);
     // });
   }, []);
 
-  React.useEffect(() => {
+  // sync cart with local storage
+  useEffect(() => {
     if (isSync) return;
     const storedCart = localStorage.getItem("cart");
     if (storedCart) {
@@ -36,19 +52,30 @@ export default function useCart() {
     setIsSync(true);
   }, []);
 
-  React.useEffect(() => {
-    const sum = cart.reduce((acc, item) => {
+  // update total price
+  useEffect(() => {
+    const sum = cart.reduce((prev, item) => {
       const product = products.find((product) => product.id === item.id);
       if (product) {
-        return acc + product.price * item.quantity;
+        return prev + product.price * item.quantity;
       } else {
-        return acc;
+        return prev;
       }
     }, 0);
-    setTotalPrice(sum);
+
+    // setTotalPrice(sum);
+
+    // dispatch({
+    //   type: "UPDATE_TOTAL_PRICE",
+    //   payload: {
+    //     total: sum,
+    //   },
+    // });
+
+    onUpdateTotalPrice(sum);
 
     localStorage.setItem("cart", JSON.stringify(cart));
-  }, [cart, products]);
+  }, [cart, products, onUpdateTotalPrice]);
 
   function handleAddToCart(product: ProductType) {
     updateCart((draft) => {
@@ -103,10 +130,117 @@ export default function useCart() {
     totalPrice,
     cart,
     updateCart,
-    setTotalPrice,
     isSync,
     setIsSync,
     products,
     setProducts,
   };
 }
+
+// export default function useCart() {
+//   const [cart, updateCart] = useImmer<CartType[]>([]);
+//   const [totalPrice, setTotalPrice] = React.useState(0);
+//   const [isSync, setIsSync] = React.useState(false);
+//   const [products, setProducts] = React.useState<ProductType[]>([]);
+
+//   React.useEffect(() => {
+//     async function exec() {
+//       try {
+//         const data = await getProducts();
+//         setProducts(data);
+//       } catch (error: any) {
+//         alert(error.message);
+//         setProducts([]);
+//       }
+//     }
+//     exec();
+//     // getProducts((data) => {
+//     //   setProducts(data);
+//     // });
+//   }, []);
+
+//   React.useEffect(() => {
+//     if (isSync) return;
+//     const storedCart = localStorage.getItem("cart");
+//     if (storedCart) {
+//       updateCart((draft) => {
+//         draft.push(...JSON.parse(storedCart));
+//       });
+//     }
+//     setIsSync(true);
+//   }, []);
+
+//   React.useEffect(() => {
+//     const sum = cart.reduce((acc, item) => {
+//       const product = products.find((product) => product.id === item.id);
+//       if (product) {
+//         return acc + product.price * item.quantity;
+//       } else {
+//         return acc;
+//       }
+//     }, 0);
+//     setTotalPrice(sum);
+
+//     localStorage.setItem("cart", JSON.stringify(cart));
+//   }, [cart, products]);
+
+//   function handleAddToCart(product: ProductType) {
+//     updateCart((draft) => {
+//       const currentItem = draft.find((item) => item.id === product.id);
+//       if (currentItem) {
+//         currentItem.quantity += 1;
+//       } else {
+//         draft.push({ id: product.id, title: product.title, quantity: 1 });
+//       }
+//     });
+//   }
+
+//   function handleIncreaseQuantity(cart: CartType) {
+//     updateCart((draft) => {
+//       const currentItem = draft.find((item) => item.id === cart.id);
+//       if (currentItem) {
+//         currentItem.quantity += 1;
+//       }
+//     });
+//   }
+
+//   function removeItem(draft: CartType[], cart: CartType) {
+//     const index = draft.findIndex((item) => item.id === cart.id);
+//     if (index !== -1) {
+//       draft.splice(index, 1);
+//     }
+//   }
+
+//   function handleDecreaseQuantity(cart: CartType) {
+//     updateCart((draft) => {
+//       const currentItem = draft.find((item) => item.id === cart.id);
+//       if (currentItem && currentItem.quantity > 1) {
+//         currentItem.quantity -= 1;
+//       } else {
+//         removeItem(draft, cart);
+//       }
+//     });
+//   }
+
+//   function handleRemoveFromCart(cart: CartType) {
+//     updateCart((draft) => {
+//       removeItem(draft, cart);
+//       // draft.splice(draft.indexOf(cart), 1);
+//     });
+//   }
+
+//   return {
+//     handleAddToCart,
+//     handleIncreaseQuantity,
+//     handleDecreaseQuantity,
+//     handleRemoveFromCart,
+//     totalPrice,
+//     cart,
+//     updateCart,
+//     setTotalPrice,
+//     isSync,
+//     setIsSync,
+//     products,
+//     setProducts,
+//   };
+// }
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,157 +1,18 @@
-// import { useState } from "react";
-import { useImmer } from "use-immer";
-import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Product from "../components/Product";
 import Cart from "../components/Cart";
-import { getProducts, ProductType } from "../services/product.service";
-import { useLogin } from "../hooks/useLogin";
-import useTotalPrice from "../hooks/use-total-price";
-
-export type CartType = {
-  id: number;
-  title: string;
-  quantity: number;
-};
-
-// const products: ProductType[] = [
-//   {
-//     id: 1,
-//     name: "Sepatu Bagus",
-//     price: 100000,
-//     imageUrl: "/images/fashion-shoes-sneakers.jpg",
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,recusandae rerum impedit explicabo dolores in perspiciatis ipsum quod doloribus quisquam Provident earum enim dolor maxime quae, nobis dicta numquam voluptatum",
-//   },
-//   {
-//     id: 2,
-//     name: "Sepatu Keren",
-//     price: 10000000,
-//     imageUrl: "/images/fashion-shoes-sneakers.jpg",
-//     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-//   },
-//   {
-//     id: 3,
-//     name: "Sepatu Mantap",
-//     price: 1000000,
-//     imageUrl: "/images/fashion-shoes-sneakers.jpg",
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit.  in perspiciatis ipsum quod doloribus quisquam Provident earum enim dolor maxime quae, nobis dicta numquam voluptatum",
-//   },
-// ];
+import useCart from "../hooks/use-cart";
 
 export default function ProductsPage() {
-  const [cart, updateCart] = useImmer<CartType[]>([]);
-  // const [totalPrice, setTotalPrice] = useState(0);
-  const [isSync, setIsSync] = useState(false);
-  const [products, setProducts] = useState<ProductType[]>([]);
-
-  // const dispatch = useTotalPriceDispatch();
-  // const { total } = useTotalPrice();
-
-  const { totalPrice, onUpdateTotalPrice } = useTotalPrice();
-
-  useLogin();
-
-  // get product from api
-  useEffect(() => {
-    async function exec() {
-      try {
-        const data = await getProducts();
-        setProducts(data);
-      } catch (error: any) {
-        alert(error.message);
-        setProducts([]);
-      }
-    }
-    exec();
-
-    // getProducts((data) => {
-    //   setProducts(data);
-    // });
-  }, []);
-
-  // sync cart with local storage
-  useEffect(() => {
-    if (isSync) return;
-    const storedCart = localStorage.getItem("cart");
-    if (storedCart) {
-      updateCart((draft) => {
-        draft.push(...JSON.parse(storedCart));
-      });
-    }
-    setIsSync(true);
-  }, []);
-
-  // update total price
-  useEffect(() => {
-    const sum = cart.reduce((prev, item) => {
-      const product = products.find((product) => product.id === item.id);
-      if (product) {
-        return prev + product.price * item.quantity;
-      } else {
-        return prev;
-      }
-    }, 0);
-
-    // setTotalPrice(sum);
-
-    // dispatch({
-    //   type: "UPDATE_TOTAL_PRICE",
-    //   payload: {
-    //     total: sum,
-    //   },
-    // });
-
-    onUpdateTotalPrice(sum);
-
-    localStorage.setItem("cart", JSON.stringify(cart));
-  }, [cart, products, onUpdateTotalPrice]);
-
-  function handleAddToCart(product: ProductType) {
-    updateCart((draft) => {
-      const currentItem = draft.find((item) => item.id === product.id);
-      if (currentItem) {
-        currentItem.quantity += 1;
-      } else {
-        draft.push({ id: product.id, title: product.title, quantity: 1 });
-      }
-    });
-  }
-
-  function handleIncreaseQuantity(cart: CartType) {
-    updateCart((draft) => {
-      const currentItem = draft.find((item) => item.id === cart.id);
-      if (currentItem) {
-        currentItem.quantity += 1;
-      }
-    });
-  }
-
-  function removeItem(draft: CartType[], cart: CartType) {
-    const index = draft.findIndex((item) => item.id === cart.id);
-    if (index !== -1) {
-      draft.splice(index, 1);
-    }
-  }
-
-  function handleDecreaseQuantity(cart: CartType) {
-    updateCart((draft) => {
-      const currentItem = draft.find((item) => item.id === cart.id);
-      if (currentItem && currentItem.quantity > 1) {
-        currentItem.quantity -= 1;
-      } else {
-        removeItem(draft, cart);
-      }
-    });
-  }
-
-  function handleRemoveFromCart(cart: CartType) {
-    updateCart((draft) => {
-      removeItem(draft, cart);
-      // draft.splice(draft.indexOf(cart), 1);
-    });
-  }
+  const {
+    products,
+    handleAddToCart,
+    cart,
+    handleIncreaseQuantity,
+    handleDecreaseQuantity,
+    handleRemoveFromCart,
+    totalPrice,
+  } = useCart();
 
   return (
     <>
